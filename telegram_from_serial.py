d6d1a7f13d366d93fd9b03714535c12e931704e2
Jan van Haarst <jan@vanhaarst.net>
2014-09-08 23:22:36 +0200

    Only show interesting lines, and pretty print them.

--- telegram_from_serial.py
+++ telegram_from_serial.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python2
 # Python script to retrieve and parse a DSMR telegram from a P1 port
 
-
 import re
 import sys
 import serial
 import crcmod.predefined
 
-import pprint
-
 # Debugging settings
 production = True   # Use serial or file as input
 debugging = 1   # Show extra output
 # DSMR interesting codes
-list_of_interesting_codes = dict()
+list_of_interesting_codes = {
+    '1-0:1.8.1': 'Meter Reading electricity delivered to client (Tariff 1) in kWh',
+    '1-0:1.8.2': 'Meter Reading electricity delivered to client (Tariff 2) in kWh',
+    '1-0:2.8.1': 'Meter Reading electricity delivered by dient (Tariff 1) in kWh',
+    '1-0:2.8.2': 'Meter Reading electricity delivered by client (Tariff 2) in kWh',
+    '0-0:96.14.0': 'Tariff indicator electricity',
+    '1-0:1.7.0': 'Actual electricity power delivered (+P) in kgitW',
+    '1-0:2.7.0': 'Actual electricity power received (-P) in kW',
+    '0-0:17.0.0': 'The actual threshold electricity in kW',
+    '0-0:96.3.10': 'Switch position electricity',
+    '0-0:96.7.21': 'Number of power failures in any phase',
+    '0-0:96.7.9': 'Number of long power failures in any phase',
+    '1-0:32.32.0': 'Number of voltage sags in phase L1',
+    '1-0:52.32.0': 'Number of voltage sags in phase L2',
+    '1-0:72:32.0': 'Number of voltage sags in phase L3',
+    '1-0:32.36.0': 'Number of voltage swells in phase L1',
+    '1-0:52.36.0': 'Number of voltage swells in phase L2',
+    '1-0:72.36.0': 'Number of voltage swells in phase L3',
+    '1-0:31.7.0': 'Instantaneous current L1 in A',
+    '1-0:51.7.0': 'Instantaneous current L2 in A',
+    '1-0:71.7.0': 'Instantaneous current L3 in A',
+    '1-0:21.7.0': 'Instantaneous active power L1 (+P) in kW',
+    '1-0:41.7.0': 'Instantaneous active power L2 (+P) in kW',
+    '1-0:61.7.0': 'Instantaneous active power L3 (+P) in kW',
+    '1-0:22.7.0': 'Instantaneous active power L1 (-P) in kW',
+    '1-0:42.7.0': 'Instantaneous active power L2 (-P) in kW',
+    '1-0:62.7.0': 'Instantaneous active power L3 (-P) in kW'
+}
+
 # Program variables
 # The true telegram ends with an exclamation mark after a CR/LF
 pattern = re.compile(b'\r\n(?=!)')
@@ -98,7 +123,7 @@ while True:
             print("Good checksum !")
         # Store the vaules in a dictionary
         telegram_values = dict()
-        # Split the telegram back into lines and iterate over them
+        # Split the telegram into lines and iterate over them
         for telegram_line in telegram.split(b'\r\n'):
             # Split the OBIS code from the value
             # The lines with a OBIS code start with a number
@@ -117,12 +142,10 @@ while True:
                 value = ''.join(re.split(b'(\()', telegram_line)[1:])
                 telegram_values[code] = value
 
-        for code in telegram_values:
-            #print code, 'has', telegram_values[code]
+        # Print the lines to screen
+        for code, value in sorted(telegram_values.items()):
             if code in list_of_interesting_codes:
                 # Cleanup value
-                # Print nicely formatted string with clean value
-                # Push to Xively
-                pass
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(telegram_values)
+                value = float(value.lstrip(b'\(').rstrip(b'\)*kWhA'))
+                # Print nicely formatted string
+                print("{0:<63}{1:>8}".format(list_of_interesting_codes[code], value))
