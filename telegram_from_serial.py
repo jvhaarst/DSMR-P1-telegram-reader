d36879b906d7743c2140ff56e5c5a2f07412806c
Franf Reerink <freerink@gmail.com>
2022-03-01 17:51:39 +0100

    port to Python 3

--- telegram_from_serial.py
+++ telegram_from_serial.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # Python script to retrieve and parse a DSMR telegram from a P1 port
 
 import re
@@ -6,6 +6,7 @@ import sys
 import serial
 import crcmod.predefined
 import datetime
+import json
 
 # Debugging settings
 production = True   # Use serial or file as input
@@ -13,49 +14,52 @@ debugging = 1   # Show extra output
 # DSMR interesting codes
 gas_meter = '1' 
 list_of_interesting_codes = {
-    '1-0:1.8.1': 'Meter Reading electricity delivered to client (Tariff 1) in kWh',
-    '1-0:1.8.2': 'Meter Reading electricity delivered to client (Tariff 2) in kWh',
-    '1-0:2.8.1': 'Meter Reading electricity delivered by client (Tariff 1) in kWh',
-    '1-0:2.8.2': 'Meter Reading electricity delivered by client (Tariff 2) in kWh',
-    '0-0:96.14.0': 'Tariff indicator electricity',
-    '1-0:1.7.0': 'Actual electricity power delivered (+P) in kW',
-    '1-0:2.7.0': 'Actual electricity power received (-P) in kW',
-    '0-0:17.0.0': 'The actual threshold electricity in kW',
-    '0-0:96.3.10': 'Switch position electricity',
-    '0-0:96.7.21': 'Number of power failures in any phase',
-    '0-0:96.7.9': 'Number of long power failures in any phase',
-    '1-0:32.32.0': 'Number of voltage sags in phase L1',
-    '1-0:52.32.0': 'Number of voltage sags in phase L2',
-    '1-0:72:32.0': 'Number of voltage sags in phase L3',
-    '1-0:32.36.0': 'Number of voltage swells in phase L1',
-    '1-0:52.36.0': 'Number of voltage swells in phase L2',
-    '1-0:72.36.0': 'Number of voltage swells in phase L3',
-    '1-0:31.7.0': 'Instantaneous current L1 in A',
-    '1-0:51.7.0': 'Instantaneous current L2 in A',
-    '1-0:71.7.0': 'Instantaneous current L3 in A',
-    '1-0:21.7.0': 'Instantaneous active power L1 (+P) in kW',
-    '1-0:41.7.0': 'Instantaneous active power L2 (+P) in kW',
-    '1-0:61.7.0': 'Instantaneous active power L3 (+P) in kW',
-    '1-0:22.7.0': 'Instantaneous active power L1 (-P) in kW',
-    '1-0:42.7.0': 'Instantaneous active power L2 (-P) in kW',
-    '1-0:62.7.0': 'Instantaneous active power L3 (-P) in kW',
-    '0-'+gas_meter+':24.2.1': 'Last hourly value (temperature converted), gas delivered to client in m3'
+    '1-0:1.8.1': ['Meter Reading electricity delivered to client (Tariff 1) in kWh', 'totalPowerDeliveredToClientTariff1'],
+    '1-0:1.8.2': ['Meter Reading electricity delivered to client (Tariff 2) in kWh', 'totalPowerDeliveredToClientTariff2'],
+    '1-0:2.8.1': ['Meter Reading electricity delivered by client (Tariff 1) in kWh', ''],
+    '1-0:2.8.2': ['Meter Reading electricity delivered by client (Tariff 2) in kWh', ''],
+    '0-0:96.14.0': ['Tariff indicator electricity', 'tariffIndicator'],
+    '1-0:1.7.0': ['Actual electricity power delivered (+P) in kW', 'actualPowerDelivered'],
+    '1-0:2.7.0': ['Actual electricity power received (-P) in kW', ''],
+    '0-0:17.0.0': ['The actual threshold electricity in kW', 'threshold'],
+    '0-0:96.3.10': ['Switch position electricity', 'switch'],
+    '0-0:96.7.21': ['Number of power failures in any phase', 'failures'],
+    '0-0:96.7.9': ['Number of long power failures in any phase', 'longFailures'],
+    '1-0:32.32.0': ['Number of voltage sags in phase L1', ''],
+    '1-0:52.32.0': ['Number of voltage sags in phase L2', ''],
+    '1-0:72:32.0': ['Number of voltage sags in phase L3', ''],
+    '1-0:32.36.0': ['Number of voltage swells in phase L1', ''],
+    '1-0:52.36.0': ['Number of voltage swells in phase L2', ''],
+    '1-0:72.36.0': ['Number of voltage swells in phase L3', ''],
+    '1-0:31.7.0': ['Instantaneous current L1 in A', 'actualCurrentL1'],
+    '1-0:32.7.0': ['Instantaneous voltage L1 in V', 'actualVoltageL1'],
+    '1-0:51.7.0': ['Instantaneous current L2 in A', ''],
+    '1-0:71.7.0': ['Instantaneous current L3 in A', ''],
+    '1-0:21.7.0': ['Instantaneous active power L1 (+P) in kW', 'actualPowerL1'],
+    '1-0:41.7.0': ['Instantaneous active power L2 (+P) in kW', ''],
+    '1-0:61.7.0': ['Instantaneous active power L3 (+P) in kW', ''],
+    '1-0:22.7.0': ['Instantaneous active power L1 (-P) in kW', ''],
+    '1-0:42.7.0': ['Instantaneous active power L2 (-P) in kW', ''],
+    '1-0:62.7.0': ['Instantaneous active power L3 (-P) in kW', ''],
+    '0-'+gas_meter+':24.2.1': ['gas delivered to client in m3', 'totalGasDeliveredToClient']
 }
 
-max_len = max(map(len,list_of_interesting_codes.values()))
-    
+#max_len = max(map(len,list_of_interesting_codes.values()))
+max_len = 72
+ 
 # Program variables
 # Set the way the values are printed:
-print_format = 'string'
+print_format = 'json'
+#print_format = 'string'
 # The true telegram ends with an exclamation mark after a CR/LF
-pattern = re.compile(b'\r\n(?=!)')
+pattern = re.compile('\r\n(?=!)')
 # According to the DSMR spec, we need to check a CRC16
 crc16 = crcmod.predefined.mkPredefinedCrcFun('crc16')
 # Create an empty telegram
 telegram = ''
 checksum_found = False
 good_checksum = False
-
+count = 0
 
 if production:
     #Serial port configuration
@@ -79,24 +83,27 @@ while True:
         if production:
             #Open serial port
             try:
+                if debugging == 1:
+                    print("Opening serial line, count {0}".format(count))
+                count += 1
                 ser.open()
                 telegram = ''
                 checksum_found = False
             except Exception as ex:
                 template = "An exception of type {0} occured. Arguments:\n{1!r}"
                 message = template.format(type(ex).__name__, ex.args)
-                print message
+                print(message)
                 sys.exit("Fout bij het openen van %s. Programma afgebroken." % ser.name)
         else:
             telegram = ''
             checksum_found = False
         while not checksum_found:
             # Read in a line
-            telegram_line = ser.readline()
+            telegram_line = ser.readline().decode('ascii')
             if debugging == 2:
                 print(telegram_line.decode('ascii').strip())
             # Check if it matches the checksum line (! at start)
-            if re.match(b'(?=!)', telegram_line):
+            if re.match('(?=!)', telegram_line):
                 telegram = telegram + telegram_line
                 if debugging:
                     print('Found checksum!')
@@ -107,11 +114,13 @@ while True:
     except Exception as ex:
         template = "An exception of type {0} occured. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
-        print message
+        print(message)
         print("There was a problem %s, continuing...") % ex
     #Close serial port
     if production:
         try:
+            if debugging == 1:
+                print("Closing serial line")
             ser.close()
         except:
             sys.exit("Oops %s. Programma afgebroken." % ser.name)
@@ -120,9 +129,9 @@ while True:
     for m in pattern.finditer(telegram):
         # Remove the exclamation mark from the checksum,
         # and make an integer out of it.
-        given_checksum = int('0x' + telegram[m.end() + 1:].decode('ascii'), 16)
+        given_checksum = int('0x' + telegram[m.end() + 1:], 16)
         # The exclamation mark is also part of the text to be CRC16'd
-        calculated_checksum = crc16(telegram[:m.end() + 1])
+        calculated_checksum = crc16(bytes(telegram[:m.end() + 1], 'ascii'))
         if given_checksum == calculated_checksum:
             good_checksum = True
     if good_checksum:
@@ -131,10 +140,10 @@ while True:
         # Store the vaules in a dictionary
         telegram_values = dict()
         # Split the telegram into lines and iterate over them
-        for telegram_line in telegram.split(b'\r\n'):
+        for telegram_line in telegram.split('\r\n'):
             # Split the OBIS code from the value
             # The lines with a OBIS code start with a number
-            if re.match(b'\d', telegram_line):
+            if re.match('\d', telegram_line):
                 if debugging == 3:
                     print(telegram_line)
                 # The values are enclosed with parenthesis
@@ -143,30 +152,38 @@ while True:
                 if debugging == 2:
                     print(telegram_line)
                 if debugging == 3:
-                    print re.split(b'(\()', telegram_line)
+                    print(re.split('(\()', telegram_line))
                 # You can't put a list in a dict TODO better solution
-                code = ''.join(re.split(b'(\()', telegram_line)[:1])
-                value = ''.join(re.split(b'(\()', telegram_line)[1:])
+                code = ''.join(re.split('(\()', telegram_line)[:1])
+                value = ''.join(re.split('(\()', telegram_line)[1:])
                 telegram_values[code] = value
 
+        json_values = dict()
         # Print the lines to screen
+        #            print(json.dumps(telegram_values.items(), indent = 4))
         for code, value in sorted(telegram_values.items()):
-            if code in list_of_interesting_codes:
+            if code in list_of_interesting_codes and len(list_of_interesting_codes[code][1]) > 0 :
                 # Cleanup value
                 # Gas needs another way to cleanup
                 if 'm3' in value:
                         (time,value) = re.findall('\((.*?)\)',value)
-                        value = float(value.lstrip(b'\(').rstrip(b'\)*m3'))
+                        value = float(value.lstrip('\(').rstrip('\)*m3'))
                 else:
-                        value = float(value.lstrip(b'\(').rstrip(b'\)*kWhA'))
+                        value = float(value.lstrip('\(').rstrip('\)*kWhAV'))
                 # Print nicely formatted string
                 if print_format == 'string' :
                     print_string = '{0:<'+str(max_len)+'}{1:>12}'
                     if debugging > 0:
                             print(datetime.datetime.utcnow()),
-                    print(print_string.format(list_of_interesting_codes[code], value))
+                    print(print_string.format(list_of_interesting_codes[code][0], value))
+                elif print_format == 'json' :
+                    json_values[list_of_interesting_codes[code][1]] = value
                 else:
                     print_string = '{0:<10}{1:>12}'
                     if debugging > 0:
                             print(datetime.datetime.utcnow()),
                     print(print_string.format(code, value))
+        if print_format == 'json' :
+            print(json.dumps(json_values, indent = 4))
+    else:
+        print("Bad checksum")
